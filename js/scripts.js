35e8ccb2dd74658a70e664ef9ec36804218318da
adriankiprono <tuimuradrian6@gmail.com>
2019-09-22 22:13:59 +0300

    add input in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,29 @@
+function dayCalculator() {
+  //get input
 
+
+
+  var c-number =document.getElementById('century');
+  var y-number =document.getElementById('year');
+  var m-number =document.getElementById('month');
+  var d-number =document.getElementById('day');
+
+  var errMsg ="";
+  if (century< 0 )
+  {
+    errMsg=errMsg + "invalid"
+  }
+  if (year< 0)
+  {
+    errMsg=errMsg + "invalid"
+  }
+  if (month< 0)
+  {
+    errMsg=errMsg + "invalid"
+  }
+  if (day< 0)
+  {
+    errMsg=errMsg + "invalid"
+  }
+  else 
+}
