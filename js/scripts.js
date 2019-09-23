74211b63762acb0b308db406be5e5334797a44ae
adriankiprono <tuimuradrian6@gmail.com>
2019-09-23 10:52:45 +0300

    add arrays of dfemale and male names

--- js/scripts.js
+++ js/scripts.js
@@ -8,35 +8,4 @@ function dayCalculator() {
   var MM=document.getElementById('month');
   var DD =document.getElementById('day');
 
-    var errMsg ="";
-    if (CC.value< 0 )
-    {
-      errMsg=errMsg + "invalid";
-      alert("invalid");
-    }
-    if (YY.value< 0)
-    {
-      errMsg=errMsg + "invalid";
-      alert("invalid");
-    }
-    if (MM.value< 0)
-    {
-      errMsg=errMsg + "invalid";
-      alert("invalid");
-    }
-    if (DD.value< 0)
-    {
-      errMsg=errMsg + "invalid";
-      alert("invalid");
-    }
-    else {
-
-   var akan =( ( (CC.value/4) -2*CC.value-1) + ((5*YY.value/4) ) + ((26*(MM.value+1)/10)) + DD.value ) % 7;
-  alert(akan);
-
-  }
-
-}
- var dayOfTheWeek =("Monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday");
- var femaleName =("Akosua", "Adwoa","Abenaa", "Akua",  "Yaa", "Afua", "Ama");
- var maleName =( "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame");
+  
